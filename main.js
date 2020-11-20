87869e458cf2db25a8a8c03d67e866e8cd2d2eec
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2020-11-20 08:34:19 +0300

    Урок №2

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 'use strict';
-let money;
 
-let income;
+let money = 50000;
+console.log(typeof money);
 
-let addExpenses;
+let income = 'фриланс';
+console.log(typeof income);
 
-let deposit;
+let deposit = true;
+console.log(typeof deposit);
 
-let mission;
+let addExpenses = 'Интернет, Такси, Коммуналка';
+console.log(addExpenses.length);
 
-let period;
+let mission = 150000;
+let period = 3;
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase().split(", "));
+
+let budgetDay = money/30;
+console.log(budgetDay);
 
 alert('Привет мир');
 console.log('Привет мир');
