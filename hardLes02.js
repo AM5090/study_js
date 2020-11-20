ea843fc4f30be5a8f9a4d5471fa696d6e1921ec1
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2020-11-20 20:59:46 +0300

    Усложненный урок №2 через reduce();

--- hardLes02.js
+++ hardLes02.js
@@ -2,6 +2,7 @@
 
 let num = 266219;
 num = String(num);
+
 let n = 1;
 
 for(let i = 0; i < num.length; i++){
@@ -12,3 +13,12 @@ let m = n ** 3;
 
 m = String(m);
 console.log(m.substring(0,2));
+
+// === reduce();
+console.log("===");
+num = num.split('');
+
+let result = num.reduce((mult, item) => mult * item, 1);
+result = result ** 3;
+
+console.log(String(result).substring(0,2));
