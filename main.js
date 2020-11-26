cb7d5f52f45d773143a2e322ecfe52f81648d942
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2020-11-26 18:44:39 +0300

    Урок №6 усложненный

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ function isNumber(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+
 function game(){
 
+    let count = 10;
     const randNum = Math.floor(Math.random() * 100 + 1);
 
     function guessNumber() {
-        let yourNum = prompt('Угадай число от 1 до 100');
+        let yourNum = prompt('Угадай число от 1 до 100, количество попыток - ' + count);
         
         if(yourNum === null){
             alert('Игра окончена');
@@ -21,18 +23,42 @@ function game(){
             }
 
             if(parseFloat(yourNum) > randNum) {
-                alert('Загаданное число меньше');
-                guessNumber();
+                count--;
+                if(count > 0){
+                    alert('Загаданное число меньше, осталось попыток ' + count);
+                    guessNumber();
+                }else if(count === 0){
+                    let a = confirm('Попытки закончились, хотите сыграть еще?');
+                    if(a){
+                        game();
+                    }else if(a === false){
+                        alert('Игра окончена');
+                    }
+                }
             }else if(parseFloat(yourNum) < randNum) {
-                alert('Загаданное число больше');
-                guessNumber();
+                count--;
+                if(count > 0){
+                    alert('Загаданное число больше, осталось попыток ' + count);
+                    guessNumber();
+                }else if(count === 0){
+                    let b = confirm('Попытки закончились, хотите сыграть еще?');
+                    if(b){
+                        game();
+                    }else if(b === false){
+                        alert('Игра окончена');
+                    }
+                }
             }else if(parseFloat(yourNum) === randNum){
-                alert('Поздравляю, Вы угадали!!!');
+                let win = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                if(win){
+                    game();
+                }else if(win === false){
+                    alert('Игра окончена');
+                }
             }
         }
     }
     guessNumber();
-
 }
 
 game();
