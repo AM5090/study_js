c2d496f74f15774f196856b9cc47a1beda27990f
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2020-12-16 01:03:45 +0300

    Урок №19

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-
+    //таймер
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
@@ -35,9 +35,72 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
 
+        updateClock();
         idInterval = setInterval(updateClock, 1000);
     }
 
     countTimer('16 december 2020');
+
+    //меню
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        //открытие и закрытие меню
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+    };
+    toggleMenu();
+
+    //поп-ап окно
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+        //анимация поп-ап окна
+        let count = -400;
+        let interval;
+        const popupAnimate = () => {
+            if (screen.width > 768) {
+                interval = requestAnimationFrame(popupAnimate);
+                count += 8;
+
+                popupContent.style.top = count + 'px';
+                if (popupContent.style.top === 200 + 'px') { 
+                    cancelAnimationFrame(interval);
+                    count = -400;
+                }
+            } else {
+                cancelAnimationFrame(interval);
+                popup.style.display = 'block';
+                popupContent.style.top = 'block';
+            }
+        };
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                interval = requestAnimationFrame(popupAnimate);
+                popup.style.display = 'block';
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    togglePopup();
 });
 
