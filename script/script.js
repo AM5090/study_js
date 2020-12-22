091139f31657011bacec942320dc6408a63803c6
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2020-12-22 19:19:17 +0300

    Урок №24

--- script/script.js
+++ script/script.js
@@ -275,18 +275,54 @@ window.addEventListener('DOMContentLoaded', () => {
     dataSrc();
 
     // калькулятор
-    const calculet = () => {
-        const calcBlock = document.querySelector('.calc-block');
+    const calculet = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
 
 
-        calcBlock.addEventListener('input', event => {
-            const target = event.target.closest('input');
-            target.value = target.value.replace(/\D/g, '');
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+                if (target.matches('input')) {
+                    target.value = target.value.replace(/\D/g, '');
+                }
+            }
 
         });
     };
 
-    calculet();
+    calculet(100);
 
 });
 
