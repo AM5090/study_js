81fa85b225dc48d15c5a9d624eae498df68746cf
Armen Melkonyan <armen.melkonyan.93@inbox.ru>
2021-01-14 22:02:47 +0300

    Реализовал слайдер(5 пункт)

--- script.js
+++ script.js
@@ -64,4 +64,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     scrollContent();
 
+    // Слайдер
+    const slider = () => {
+
+        const items = document.querySelectorAll('.item');
+
+        items.forEach((item, i) => {
+            item.style.opacity = 0;
+            item.style.position = 'absolute';
+            item.style.width = '100%';
+
+            const table = items[i].querySelector('.table');
+            table.style.opacity = 1;
+            table.style.visibility = 'visible';
+        });
+
+        let count = 0;
+
+        setInterval(() => {
+            items[count].style.opacity = 1;
+
+            if (count === 0) {
+                items[items.length - 1].style.opacity = 0;
+            } else if (count > 0) {
+                items[count - 1].style.opacity = 0;
+            }
+
+            count++;
+
+            if (count === items.length) {
+                count = 0;
+            }
+
+        }, 3000);
+    };
+
+    slider();
 });
